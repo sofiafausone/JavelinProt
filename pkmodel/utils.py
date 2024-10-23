c2d621732d3b92d511b44e3e700c4347379897d1
Hew <hew.phipps@keble.ox.ac.uk>
2024-10-23 11:52:35 +0100

    Created a Config class to read config .yaml files by parsing them into a nested dictionary

---
+++ pkmodel/utils.py
@@ -0,0 +1,65 @@
+'''
+
+'''
+
+
+class Stdout:
+
+    '''
+
+    '''
+
+    def __init__(fself, ile):
+
+        self.file = file
+
+    def write(self, msg):
+
+        '''
+
+        '''
+
+        with open (self.file, 'a') as f:
+            f.write(msg)
+
+        print(msg)
+
+
+
+class Config:
+
+    '''
+
+    '''
+
+    def __init__(self, _id):
+
+        self.id = _id
+
+
+    def read_yaml(self, file):
+
+        '''
+
+        '''
+
+        with open(file, "r") as f:
+
+            dict = {}
+
+            for line in f:
+                if len(line.strip()) > 0:
+
+                    if line.startswith(" "):
+                        name, value = line.strip().split(": ")
+                        dict[section][name] = value
+                
+                    else:
+                        section = line.strip().rstrip(":")
+           
+                else:
+                    continue
+
+    self.dict = dict
+    
+    return dict
