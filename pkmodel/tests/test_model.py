944dca9a181eac6d46d933908d548611beedad4c
SofiaFausone <sofia.fausone@univ.ox.ac.uk>
2024-10-24 10:27:56 +0100

    trying to merge with Hew's new version

--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -1,5 +1,8 @@
 import unittest
 import pkmodel as pk
+import numpy as np 
+import numpy.testing as npt
+
 
 
 class ModelTest(unittest.TestCase):
@@ -12,4 +15,5 @@ class ModelTest(unittest.TestCase):
         """
         model = pk.Model()
         self.assertEqual(model.value, 42)
+
 
